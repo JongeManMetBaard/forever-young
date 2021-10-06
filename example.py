edb64446895e538f3197affca2a53381fb77e738
ali <ali.gout.12@hotmail.com>
2021-10-06 09:50:39 +0200

    'test'

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 3')
+robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
-for i in range(4):
-    robotArm.grab()
-    robotArm.moveRight();
-    robotArm.drop()
-    robotArm.moveLeft()
+
+robotArm.moveRight();
+robotArm.grab()
+robotArm.moveLeft();
+robotArm.drop();
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
